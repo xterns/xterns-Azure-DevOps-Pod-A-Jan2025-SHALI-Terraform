e538c50f3e7ee884d77aaa0e97e1834d064c9b83
idokochukwudie <idokochukwudie@gmail.com>
2025-03-18 09:35:53 +0100

    Modified SOURCE reference in the ROOT main.tf 'modules' to module'

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vnet" {
-  source              = "./modules/vnet"
+  source              = "./module/vnet"
   vnet_name           = var.vnet_name
   location            = var.location
   resource_group_name = "externs-pod" # Now this will work!
