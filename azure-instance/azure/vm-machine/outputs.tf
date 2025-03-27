ffc88d7016f4fbfb3757e8394b7b5f61e2234e95
Akindiya Olalekan <lehqandiya@gmail.com>
2025-03-27 21:13:27 +0100

    Delete azure-instance directory

--- azure-instance/azure/vm-machine/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_id" {
-  value       = azurerm_linux_virtual_machine.vm.id
-  description = "The ID of the created VM"
-}
-
-output "vm_private_ip" {
-  value       = azurerm_network_interface.nic.private_ip_address
-  description = "Private IP address of the VM"
-}
